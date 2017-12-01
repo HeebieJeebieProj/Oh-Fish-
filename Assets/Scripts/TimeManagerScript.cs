4f3194eb34db420652b46a64e83f2b813094de53
HeebieJeebieProj <vividkaizoku@gmail.com>
2017-12-02 04:37:18 +0530

    Minor fixes

--- Assets/Scripts/TimeManagerScript.cs
+++ Assets/Scripts/TimeManagerScript.cs
@@ -12,11 +12,14 @@ public class TimeManagerScript : MonoBehaviour {
     public static float timeOfDay; //public accessible float storing exact time of the day in seconds
     public static int day; //public accessible day count
 
+    public float hour; //To show hour in inspector
+
 	// Use this for initialization
 	void Start () {
         speed = 86400 / (timeTotalMin * 60);
         timeOfDay = initTimeHr * 60 * 60;
         day = 0;
+        hour = initTimeHr;
 	}
 	
 	// Update is called once per frame
@@ -25,11 +28,19 @@ public class TimeManagerScript : MonoBehaviour {
         // Update time of day according to speed
         timeOfDay += Time.deltaTime * speed;
 
+        //Update hour
+        hour = timeOfDay / 3600;
+
         //Day update
-        if (timeOfDay + (initTimeHr * 60 * 60) > 84600)
+        if (timeOfDay - (initTimeHr * 60 * 60) >= 84600)
+        {
+            day++;
+        }
+
+        //reset time of day at 12 am
+        if (timeOfDay >= 84600)
         {
             timeOfDay = 0;
-            day++;
         }
 
 	}
