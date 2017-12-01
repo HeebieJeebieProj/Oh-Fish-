5481b52c8e59de472bf061af4d2d93d8c22b8fe9
Shimona Elora <umysharma@gmail.com>
2017-12-02 01:43:19 +0530

    Sun path script add

---
+++ Assets/Scripts/SunPathScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunPathScript : MonoBehaviour {
+    
+    public float startY;
+    public float startX;
+    public float endY;
+    public float endX;
+
+    private float y;
+    private float x;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Transform>().position = new Vector3(startX, startY, GetComponent<Transform>().position.z);
+        y = startY;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        y += 0.01f;
+        x = -(15f / 144f) * (y + 5) * (y + 5) + 7;
+        GetComponent<Transform>().position = new Vector3(x, y, GetComponent<Transform>().position.z);
+	}
+}
