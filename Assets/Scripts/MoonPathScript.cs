3695fb819eb21ae44528ec0b7168da0208a81c61
HeebieJeebieProj <vividkaizoku@gmail.com>
2017-12-02 03:14:37 +0530

    Moon and path script add

--- Assets/Scripts/MoonPathScript.cs
+++ Assets/Scripts/MoonPathScript.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoonPathScript : MonoBehaviour {
+public class MoonPathScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float startY; //where the sun starts from
+    public float endY; //where the sun should reset it's position in terms of y
+
+    //For calculating intermediate position
+    private float y;
+    private float x;
+
+    // Use this for initialization
+    void Start()
+    {
+        y = startY;
+        GetComponent<Transform>().position = new Vector3(Mathf.Sqrt(342.25f - (y + 5) * (y + 5)) - 12f, startY, GetComponent<Transform>().position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        y += 0.01f;
+
+        //Value of x calculated using the curve (x + 12)^2 + (y + 5)^2 = 18.5^2
+        x = Mathf.Sqrt(342.25f - (y + 5) * (y + 5)) - 12f;
+
+        //Sun position set according to the x and y values
+        GetComponent<Transform>().position = new Vector3(x, y, GetComponent<Transform>().position.z);
+
+    }
 }
