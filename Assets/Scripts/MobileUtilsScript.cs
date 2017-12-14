8a16f1d57ef122be2ce1e6a6588edc35786b056e
HeebieJeebieProj <vividkaizoku@gmail.com>
2017-12-14 21:58:05 +0530

    FPS profiling add

---
+++ Assets/Scripts/MobileUtilsScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MobileUtilsScript : MonoBehaviour
+{
+
+    private int FramesPerSec;
+    private float frequency = 1.0f;
+    private string fps;
+
+    void Start()
+    {
+        StartCoroutine(FPS());
+    }
+
+    private void Update()
+    {
+        GetComponent<Text>().text = fps;
+    }
+
+    private IEnumerator FPS()
+    {
+        for (;;)
+        {
+            // Capture frame-per-second
+            int lastFrameCount = Time.frameCount;
+            float lastTime = Time.realtimeSinceStartup;
+            yield return new WaitForSeconds(frequency);
+            float timeSpan = Time.realtimeSinceStartup - lastTime;
+            int frameCount = Time.frameCount - lastFrameCount;
+
+            // Display it
+
+            fps = string.Format("FPS: {0}", Mathf.RoundToInt(frameCount / timeSpan));
+        }
+    }
+    
+}
