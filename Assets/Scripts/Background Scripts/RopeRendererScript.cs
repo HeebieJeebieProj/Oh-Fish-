feaf3da58fe161f7dc3269c08679db867fbb03a1
HeebieJeebieProj <vividkaizoku@gmail.com>
2017-12-17 19:10:45 +0530

    UI screens setup

---
+++ Assets/Scripts/Background Scripts/RopeRendererScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeRendererScript : MonoBehaviour {
+
+    public Transform[] mastJoints;
+    public Transform[] hookHangerJoints;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < mastJoints.Length; i++)
+        {
+            mastJoints[i].GetComponent<LineRenderer>().SetPosition(0, Camera.main.ScreenToWorldPoint(mastJoints[i].position));
+            mastJoints[i].GetComponent<LineRenderer>().SetPosition(1, Camera.main.ScreenToWorldPoint(hookHangerJoints[i].position));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < mastJoints.Length; i++)
+        {
+            mastJoints[i].GetComponent<LineRenderer>().SetPosition(0, mastJoints[i].position);
+            mastJoints[i].GetComponent<LineRenderer>().SetPosition(1, hookHangerJoints[i].position);
+        }
+    }
+}
