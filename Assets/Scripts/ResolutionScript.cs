50051e832dbaabed338b8b0a1973740f5353cd4c
HeebieJeebieProj <vividkaizoku@gmail.com>
2018-01-15 00:11:09 +0530

    Fps fix

---
+++ Assets/Scripts/ResolutionScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionScript : MonoBehaviour {
+
+    public int xx = 1920;
+    public int yy = 1080;
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(xx, yy, true);
+        Camera.main.aspect = 16f / 9f;
+	}
+
+}
